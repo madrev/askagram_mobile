4df7246e83869db880393a4a04bab750442199dc
Maddie Revill <maddierevill@gmail.com>
2017-02-06 19:23:28 -0800

    Catch errors from login response

--- app/util/session_api_util.js
+++ app/util/session_api_util.js
@@ -1,4 +1,6 @@
 //TODO: handle errors https://medium.com/@yoniweisbrod/interacting-with-apis-using-react-native-fetch-9733f28566bb#.fnfwx7bgg
+import { checkStatus } from './api_util';
+
 
 export const login = user => (
   fetch('http://localhost:3000/api/session', {
@@ -8,6 +10,7 @@ export const login = user => (
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(user)
-    }).then( res => (res.json())
-  )
+  }).then(res => checkStatus(res)).then(
+     res => (res.json())
+  ).catch(e => console.log(e.response))
 );
---
+++ ios/Pods/Target Support Files/Pods-AskagramMobileTests/Pods-AskagramMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AskagramMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_AskagramMobileTests
+@end
